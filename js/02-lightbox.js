d5177004ac18185d901dde8555c31203633b87fa
Viktor-Panasiuk <vo151184pvv@gmail.com>
2022-10-16 22:11:00 +0300

    final version v1.0 Task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallerryRef = document.querySelector('.gallery');
+
+gallerryRef.innerHTML = setMarkupGallery(galleryItems);
+
+
+function setMarkupGallery(galleryObjects) {
+    return galleryObjects.map(markupItem).join('');
+}
+
+function markupItem({ preview, original, description }) {
+    const markup = `
+        <li>
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>
+        `;
+    
+    return markup;
+}
+
+const options = {
+    captions: true,
+    captionSelector: 'img',
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionDelay: 250,
+};
+const gallery = new SimpleLightbox('.gallery a', options);
